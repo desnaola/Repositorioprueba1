02e7740d4a78bb65cf2a2c3d77889f2ab6c3764d
victor <victor@192.168.0.22>
2018-09-15 13:08:45 -0300

    Modificando Repositorio Java 15/09/2018

--- src/repositorio/Repositorio.java
+++ src/repositorio/Repositorio.java
@@ -7,7 +7,7 @@ public class Repositorio {
     public static void main(String[] args) {
         System.out.println("Primer Repositorio");
         System.out.println("Hola estoy actualizando el repositorio");
-        System.out.println("Ultima actualizacion Repositorio");
+        System.out.println("Ultima actualizacion Repositorio 15/09/2018 ");
     }
     
 }
